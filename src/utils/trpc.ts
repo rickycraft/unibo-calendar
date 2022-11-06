e1efae14775411225b4d013134d0870002273ddd
Riccardo Roveri <ricky.roveri@gmail.com>
2022-11-06 23:50:17 +0100

    added cache time

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,20 +1,28 @@
-import { httpBatchLink, loggerLink } from "@trpc/client";
-import { createTRPCNext } from "@trpc/next";
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
-import superjson from "superjson";
+import { httpBatchLink, loggerLink } from "@trpc/client"
+import { createTRPCNext } from "@trpc/next"
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server"
+import superjson from "superjson"
 
-import { type AppRouter } from "../server/trpc/router/_app";
+import { type AppRouter } from "../server/trpc/router/_app"
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
+  if (typeof window !== "undefined") return "" // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
+}
 
 export const trpc = createTRPCNext<AppRouter>({
   config() {
     return {
       transformer: superjson,
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: 2 * 60 * 1000,
+            cacheTime: 10 * 60 * 1000,
+          }
+        }
+      },
       links: [
         loggerLink({
           enabled: (opts) =>
@@ -25,18 +33,18 @@ export const trpc = createTRPCNext<AppRouter>({
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
-    };
+    }
   },
   ssr: false,
-});
+})
 
 /**
  * Inference helper for inputs
  * @example type HelloInput = RouterInputs['example']['hello']
  **/
-export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterInputs = inferRouterInputs<AppRouter>
 /**
  * Inference helper for outputs
  * @example type HelloOutput = RouterOutputs['example']['hello']
  **/
-export type RouterOutputs = inferRouterOutputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>
