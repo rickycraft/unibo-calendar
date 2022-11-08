f2989e4c5f06ad9afcb1f9072f8edc0f02464800
Riccardo Roveri <ricky.roveri@gmail.com>
2022-11-09 00:41:18 +0100

    removed example files

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc"
 import { calendarRouter } from './calendar'
 import { courseRouter } from './course'
-import { exampleRouter } from "./example"
 
 export const appRouter = router({
-  example: exampleRouter,
   course: courseRouter,
   calendar: calendarRouter,
 })
