ef1209fd6c957be59eeabe5fd0480bdcf518eeac
Riccardo <ricky.roveri@gmail.com>
2022-11-16 21:53:10 +0100

    added current year in consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,15 @@
 export const SITE_HOST = 'calendar.rroveri.com'
 export const S3_URL = "https://unibo-calendar.s3.eu-south-1.amazonaws.com"
+
+const MONTH_SEPT = 10
+export const currentYear = () => {
+  const now = new Date()
+
+  if (now.getMonth() < MONTH_SEPT) {
+    // between Jan - Sep return current year
+    return now.getFullYear()
+  } else {
+    // between Oct - Dec return next year
+    return now.getFullYear() + 1
+  }
+}
